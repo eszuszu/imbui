6bba7b615e82d44b4d9f15e969e824b98384e1c7
Elizabeth Szugye <115746312+eszuszu@users.noreply.github.com>
2025-08-11 15:58:32 -0600

    feat(demo): Set up 'index.html' boilerplate

---
+++ demo/main.ts
@@ -0,0 +1,31 @@
+import {
+  BaseWebComponentMixin,
+  ElementalWebComponentMixin,
+  ReactiveWebComponentMixin,
+  infuse
+} from "@imbui/infuse";
+
+const appInfusion = infuse(
+  HTMLElement,
+  BaseWebComponentMixin,
+);
+
+export const init = customElements.define(
+  'app-',
+  class App extends appInfusion {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback(): void {
+      super.connectedCallback?.();
+  
+      const slot: HTMLSlotElement = document.createElement('slot');
+      this.shadowRoot.appendChild(slot);
+    }
+  
+    disconnectedCallback(): void {
+      super.disconnectedCallback?.();
+    }
+  }
+);
