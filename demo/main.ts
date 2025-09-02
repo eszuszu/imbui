81e385d1cc5068d5bf8b0952cf6881d8cf1e783e
Elizabeth Szugye <115746312+eszuszu@users.noreply.github.com>
2025-09-01 18:08:56 -0600

    create mock directory structure in demo

--- demo/main.ts
+++ demo/main.ts
@@ -1,37 +1,54 @@
 import { html, Runtime, render, TemplateResult } from "@imbui/cast";
 import {
+  ElementalWebComponentMixin,
   BaseWebComponentMixin,
   ReactiveWebComponentMixin,
   infuse,
-  Signal,
-  signal,
 } from "@imbui/infuse";
-
+import { Signal, signal } from "@imbui/pulse";
+// You can get all of these with just "@imbui/core" too, this is just showcasing the modularity,
+// no need for `imbui/core` grab what you want, or, use what you need~
 
 const appInfusion = infuse(
   HTMLElement,
-  BaseWebComponentMixin,
+  BaseWebComponentMixin, // Be aware, all components that use the BaseWebComponentMixin attach an open shadow root by default
+  ElementalWebComponentMixin,
   ReactiveWebComponentMixin,
 );
 
-const sheet = `h1 {
+const sheet = `
+      [data-projected="counter"] {
         max-width: 100dw;
         background-color: black;
         color: white;
         display: block;
         min-height: 15rem;
-      }`
+        border-radius: 8px;
+      }
+      `
 
 const appTemplate = (
   title: string | number,
   count: number,
   dynamicClass: string,
-  handler: () => void
+  handler: () => void,
+  buttonText: string
 ) => html`
-      <h1>${title}</h1>
+      <h1 class=${"hydrated"}>${title}</h1>
       <p class="${dynamicClass}">Count: ${count}</p>
-      <button onclick="${handler}">Click Me!</button>
+      <div><button onclick="${handler}">${buttonText}</button></div>
       `;
+
+const appSlots = () => html`
+<slot name="header-slot"></slot>
+<slot name="intro-slot"></slot>
+<slot name="basics-slot"></slot>
+<slot name="pre-slot"></slot>
+<slot name="reactive-slot"></slot>
+<slot name="state-slot"></slot>
+<slot name="content-slot"></slot>
+<slot name="footer-slot"></slot>
+`
 
 export const init = customElements.define(
   'app-',
@@ -43,7 +60,8 @@ export const init = customElements.define(
     sheet!: CSSStyleSheet;
     template!: () => TemplateResult;
     dynamicClass: string;
-    constructor() {
+    buttonText: Signal<string>;
+    constructor() { // generally you want to do things in the connectedCallback, but dependency injection can happen here as long as it doesn't depend on DOM, be careful about async and microtasks
       super();
       this.globalSignal = signal<number>(0);
       this.titleSignal = signal<string | number>('hello :) Why dont you click :D \n');
@@ -52,52 +70,60 @@ export const init = customElements.define(
       this.sheet.replaceSync(sheet);
       this.adoptedStyleSheets = [this.sheet];
       this.dynamicClass = 'test';
-
+      this.buttonText = signal<string>('Click Me!');
     }
     // base mixin always attaches to a shadow dom,
     // so we need to make a slot element if we want the original
     // pre-rendered light dom to render into the shadow dom.
     connectedCallback(): void {
       super.connectedCallback?.();
-      const container = document.createElement('div');
-      const projected = document.createElement('slot');
-      this.shadowRoot.appendChild(projected);
-      this.shadowRoot.appendChild(container);
-      this.createEffect(() => {
+      render(appSlots(), this.shadowRoot, undefined, this.runtime);
 
+      this.cacheShadowElements();
+      this.collectUpdatableElements();
+      // the setTimeout mimics an async resource or service
+      setTimeout(() => {
+        this.createEffect(() => {
+            
         render(
           appTemplate(
             this.titleSignal.get(),
             this.globalSignal.get(),
             this.dynamicClass,
-            this.handler
-          ), container, undefined, this.runtime);
-    
-      });
-
-      for(let i = 0; i< 10; i++){
-
-        setTimeout(() => {
-          this.globalSignal.set(<number>this.globalSignal.get() + 1);
-          
-          const count = this.globalSignal.get();
-          if (count > 15) {
-            this.titleSignal.set(this.titleSignal.get() + ' ouch');
-          } else {
-            this.titleSignal.set(this.titleSignal.get() + 'Updated ' + count)
-          }
-        }, 2000 + (i*100));
-      }
+            this.handler,
+            this.buttonText.get()
+          ), this.updatableElements['counter'], undefined, this.runtime);
+        
+          });
+      }, 4000);
     }
-
+    
     handler = () => {
       this.globalSignal.set(<number>this.globalSignal.get() + 1);
       const count = this.globalSignal.get();
       if (count > 15) {
         this.titleSignal.set(this.titleSignal.get() + ' ouch');
       } else {
-        this.titleSignal.set(this.titleSignal.get() + 'Updated ' + count);
+        this.titleSignal.set(this.titleSignal.get() + 'Updated ' + count + '. ');
 
+      }
+      if (count > 20) {
+        this.titleSignal.set('Wow! I think, like, you won!')
+      }
+      if (count > 24) {
+        this.titleSignal.set("No, seriously, stop, this hurts!")
+      }
+      if (count > 25) {
+        this.dynamicClass = 'ouch';
+        this.buttonText.set('ded.')
+        render(
+          appTemplate(
+            this.titleSignal.get(),
+            this.globalSignal.get(),
+            this.dynamicClass,
+            this.handler,
+            this.buttonText.get(),
+          ), this.updatableElements['counter'], undefined, this.runtime);
       }
     }
   
